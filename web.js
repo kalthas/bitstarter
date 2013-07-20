74fac56dad1e44f31788e5e4cb9a4477b5512d9e
Lothair <anduin.lothair@gmail.com>
2013-07-20 04:47:12 +0000

    Modified to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  buf = Buffer(fs.readFileSync('index.html'));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
